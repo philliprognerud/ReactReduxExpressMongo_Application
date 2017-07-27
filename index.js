0b9b03545c4b45ace6d679e39cb882f3029ca413
Phillip Rognerud <philliprognerud2@yahoo.com>
2017-07-27 21:27:35 +0000

    refactored code

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-
-const keys = require("./config/keys");
+require("./services/passport");
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      // console.log(refreshToken);
-      // console.log(profile);
-    }
-  )
-);
-
-app.get("/", function(req, res) {
-  res.send("hi");
-});
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+require("./routes/authRoutes")(app);
 
 app.listen(process.env.PORT, process.env.IP, function() {
-  console.log("server has started", process.env.IP);
+  console.log("server has started", process.env.IP, process.env.PORT);
 });
